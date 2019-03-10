53905057a4a16e8eb08c9aff4b24fe3d2f506725
Tessi87 <stephanie.biergans@uq.net.au>
2019-03-10 16:11:07 +0100

    code and image for plot3,4

---
+++ plot3.R
@@ -0,0 +1,22 @@
+#loading necessary libraries
+library(dplyr)
+
+#loading data
+data<-read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings=c("?"))
+head(data)
+
+#adjusting dataset for visualisation
+data$Date<-as.character(data$Date)
+data$Time<-as.character(data$Time)
+data<-mutate(data,Date_Time=paste(Date,Time))
+data$Date_Time<-as.POSIXct(strptime(data$Date_Time,"%d/%m/%Y %H:%M:%S"))
+timeframe<-filter(data,Date_Time>=as.POSIXct("2007-02-01 00:00:00"),Date_Time<=as.POSIXct("2007-02-02 23:59:59"))
+
+#plotting and saving plot
+
+png(file="plot3.png",width=480,height=480,units="px")
+plot(Sub_metering_1~Date_Time,data=timeframe,type="l",xlab=c(""),ylab=c("Energy sub metering"),main=c(""))
+lines(Sub_metering_2~Date_Time,data=timeframe,col="red",xlab=c(""),main=c(""))
+lines(Sub_metering_3~Date_Time,data=timeframe,col="blue",xlab=c(""),main=c(""))
+legend("topright",col=c("black","red","blue"),c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1)
+dev.off()
